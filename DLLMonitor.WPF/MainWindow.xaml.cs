81b5455ed6ee84cc5d33dade504186d974ae52b2
Jarred Capellman <jcapellman@hotmail.com>
2021-02-06 15:11:05 -0600

    Added: WPF Project and Migration to a Library

---
+++ DLLMonitor.WPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows;
+
+using DLLMonitor.lib;
+
+namespace DLLMonitor.WPF
+{
+    public partial class MainWindow : Window
+    {
+        private readonly lib.ImageLoadMonitor _monitor;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _monitor = new ImageLoadMonitor();
+
+            _monitor.OnImageLoad += Monitor_OnImageLoad;
+
+            Closing += MainWindow_Closing;
+
+            _monitor.Start();
+        }
+
+        private void Monitor_OnImageLoad(object sender, ImageLoadMonitor.ImageLoadMonitorItem e)
+        {
+            Application.Current.Dispatcher.Invoke(new Action(() =>
+            {
+                txtBxDLLLoads.Text = txtBxDLLLoads.Text.Insert(0, $"{e.ProcessID}|{e.ThreadID}|{e.FileName}{Environment.NewLine}");
+            }));
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            _monitor.Stop();
+        }
+    }
+}
---
+++ DLLMonitor.lib/DLLMonitor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Session;
+
+namespace DLLMonitor.lib
+{
+    public class ImageLoadMonitor
+    {
+        public class ImageLoadMonitorItem
+        {
+            public string FileName { get; internal set; }
+
+            public int ProcessID { get; internal set; }
+
+            public int ThreadID { get; internal set; }
+        }
+
+        public const string DEFAULT_SESSION_NAME = nameof(DLLMonitor);
+
+        private readonly CancellationTokenSource ctSource = new CancellationTokenSource();
+
+        private TraceEventSession _session;
+
+        public event EventHandler<ImageLoadMonitorItem> OnImageLoad; 
+
+        public void Start(string sessionName = DEFAULT_SESSION_NAME)
+        {
+            Task.Run(() =>
+            {
+                _session = new TraceEventSession(sessionName);
+
+                _session.EnableKernelProvider(KernelTraceEventParser.Keywords.ImageLoad);
+
+                _session.Source.Kernel.ImageLoad += Kernel_ImageLoad;
+
+                _session.Source.Process();
+            }, ctSource.Token);
+        }
+
+        public void Stop()
+        {
+            ctSource.Cancel();
+
+            _session.Stop(true);
+        }
+
+        private void Kernel_ImageLoad(Microsoft.Diagnostics.Tracing.Parsers.Kernel.ImageLoadTraceData obj)
+        {
+            var item = new ImageLoadMonitorItem()
+            {
+                FileName = obj.FileName,
+                ProcessID = obj.ProcessID,
+                ThreadID = obj.ThreadID
+            };
+
+            OnImageLoad?.Invoke(this, item);
+        }
+    }
+}
