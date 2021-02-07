876ff47a68e0d37e5a196b76fd51052809d4dddc
Jarred Capellman <jcapellman@hotmail.com>
2021-02-07 14:22:07 -0600

    Added: ProcessStartMonitor initial work

---
+++ WET.lib/Monitors/ProcessStartMonitor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Diagnostics.Tracing.Parsers;
+
+using WET.lib.Enums;
+using WET.lib.Monitors.Base;
+
+namespace WET.lib.Monitors
+{
+    public class ProcessStartMonitor : BaseMonitor
+    {
+        public override KernelTraceEventParser.Keywords KeyWordMap => KernelTraceEventParser.Keywords.Process;
+
+        public override MonitorTypes MonitorType => MonitorTypes.ProcessStart;
+    }
+}
