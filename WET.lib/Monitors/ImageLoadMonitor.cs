e483d6c19e10206b69267695ebfd7100de1f2260
Jarred Capellman <jcapellman@hotmail.com>
2021-02-07 14:23:47 -0600

    Added: Initial hook for ImageLoadMonitor

---
+++ WET.lib/Monitors/ImageLoadMonitor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Diagnostics.Tracing.Parsers;
+
+using WET.lib.Enums;
+using WET.lib.Monitors.Base;
+
+namespace WET.lib.Monitors
+{
+    public class ImageLoadMonitor : BaseMonitor
+    {
+        public override KernelTraceEventParser.Keywords KeyWordMap => KernelTraceEventParser.Keywords.ImageLoad;
+
+        public override MonitorTypes MonitorType => MonitorTypes.ImageLoad;
+    }
+}
