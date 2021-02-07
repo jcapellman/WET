9d7fdd36e81e3f035f0909eb2825880734db04d4
Jarred Capellman <jcapellman@hotmail.com>
2021-02-07 14:20:59 -0600

    Change: Cleaned up the extension method to take an IEnuerable

--- WET.lib/Extensions/KernelTraceEvents.cs
+++ WET.lib/Extensions/KernelTraceEvents.cs
@@ -1,34 +1,13 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System.Linq;
+
 using Microsoft.Diagnostics.Tracing.Parsers;
-using WET.lib.Enums;
 
 namespace WET.lib.Extensions
 {
     public static class KernelTraceEventsExtensions
     {
-        public static KernelTraceEventParser.Keywords ToKeywords(this MonitorTypes monitorTypes)
-        {
-            var kernelFlags = KernelTraceEventParser.Keywords.None;
-
-            foreach (MonitorTypes monitorType in Enum.GetValues(typeof(MonitorTypes)))
-            {
-                if (!monitorTypes.HasFlag(monitorType))
-                {
-                    continue;
-                }
-
-                switch (monitorType)
-                {
-                    case MonitorTypes.ImageLoad:
-                        kernelFlags |= KernelTraceEventParser.Keywords.ImageLoad;
-                        break;
-                    case MonitorTypes.ProcessStart:
-                        kernelFlags |= KernelTraceEventParser.Keywords.Process;
-                        break;
-                }
-            }
-
-            return kernelFlags;
-        }
+        public static KernelTraceEventParser.Keywords ToKeywords(this IEnumerable<KernelTraceEventParser.Keywords> keywords) => 
+            keywords.Aggregate(KernelTraceEventParser.Keywords.None, (current, keyword) => current | keyword);
     }
 }
