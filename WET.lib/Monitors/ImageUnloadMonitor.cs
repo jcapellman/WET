fa4a98f59c8d313dc598ad45454af67d2ae256c4
Jarred Capellman <jcapellman@hotmail.com>
2021-02-08 18:49:17 -0600

    Added: ImageUnloadMonitor class

---
+++ WET.lib/Monitors/ImageUnloadMonitor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+
+using WET.lib.Enums;
+using WET.lib.MonitorItems;
+using WET.lib.Monitors.Base;
+
+namespace WET.lib.Monitors
+{
+    public class ImageUnloadMonitor : BaseMonitor
+    {
+        public override KernelTraceEventParser.Keywords KeyWordMap => KernelTraceEventParser.Keywords.ImageLoad;
+
+        public override MonitorTypes MonitorType => MonitorTypes.ImageUnload;
+
+        public override object ParseTraceEvent(TraceEvent eventData)
+        {
+            var obj = (Microsoft.Diagnostics.Tracing.Parsers.Kernel.ImageLoadTraceData)eventData;
+
+            return new ImageUnloadMonitorItem
+            {
+                FileName = obj.FileName,
+                ProcessID = obj.ProcessID,
+                ThreadID = obj.ThreadID
+            };
+        }
+    }
+}
