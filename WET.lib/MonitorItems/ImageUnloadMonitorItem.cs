0ef17916d1a88d09ff6bf1c58a528bbbf616649a
Jarred Capellman <jcapellman@hotmail.com>
2021-02-08 18:49:03 -0600

    Added: ImageUnloadMonitorItem class

---
+++ WET.lib/MonitorItems/ImageUnloadMonitorItem.cs
@@ -0,0 +1,11 @@
+﻿namespace WET.lib.MonitorItems
+{
+    public class ImageUnloadMonitorItem
+    {
+        public string FileName { get; internal set; }
+
+        public int ProcessID { get; internal set; }
+
+        public int ThreadID { get; internal set; }
+    }
+}
