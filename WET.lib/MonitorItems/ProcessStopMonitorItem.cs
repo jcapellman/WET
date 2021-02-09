54007745b0e8f0e5464a3768c18b73f209b01007
Jarred Capellman <jcapellman@hotmail.com>
2021-02-08 18:28:42 -0600

    Added: ProcessStopMonitorItem class

---
+++ WET.lib/MonitorItems/ProcessStopMonitorItem.cs
@@ -0,0 +1,9 @@
+﻿namespace WET.lib.MonitorItems
+{
+    public class ProcessStopMonitorItem
+    {
+        public int ProcessID { get; internal set; }
+
+        public string FileName { get; internal set; }
+    }
+}
