6ae2051e5ad95101f5e091ddc64ccd4eec04a30d
Jarred Capellman <jcapellman@hotmail.com>
2021-02-08 18:28:58 -0600

    Added: ProcessStopMonitor class

---
+++ WET.lib/Monitors/ProcessStopMonitor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+
+using WET.lib.Enums;
+using WET.lib.MonitorItems;
+using WET.lib.Monitors.Base;
+
+namespace WET.lib.Monitors
+{
+    public class ProcessStopMonitor : BaseMonitor
+    {
+        public override KernelTraceEventParser.Keywords KeyWordMap => KernelTraceEventParser.Keywords.Process;
+
+        public override MonitorTypes MonitorType => MonitorTypes.ProcessStop;
+
+        public override object ParseTraceEvent(TraceEvent eventData)
+        {
+            var obj = (Microsoft.Diagnostics.Tracing.Parsers.Kernel.ProcessTraceData)eventData;
+            
+            return new ProcessStopMonitorItem
+            {
+                FileName = obj.ImageFileName,
+                ProcessID = obj.ProcessID
+            };
+        }
+    }
+}
