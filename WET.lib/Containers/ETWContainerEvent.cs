f2fd08d871a5029fd6bd854f539eb7a790939837
Jarred Capellman <jcapellman@hotmail.com>
2021-02-10 20:27:50 -0600

    Added: ETWContainerEvent class

---
+++ WET.lib/Containers/ETWContainerEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using WET.lib.Enums;
+
+namespace WET.lib.Containers
+{
+    public class ETWEventContainerItem
+    {
+        public Guid ID { get; internal set; }
+
+        public DateTimeOffset Timestamp { get; internal set; }
+
+        public MonitorTypes MonitorType { get; internal set; }
+
+        public string JSON { get; internal set; }
+    }
+}
