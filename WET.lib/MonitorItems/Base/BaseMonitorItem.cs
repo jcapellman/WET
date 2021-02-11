45128edfd620ad66374c8746b92ce2d23d75557f
Jarred Capellman <jcapellman@hotmail.com>
2021-02-10 20:32:46 -0600

    Added: ProcessID and ProcessName properties in the BaseMonitorItem class

---
+++ WET.lib/MonitorItems/Base/BaseMonitorItem.cs
@@ -0,0 +1,9 @@
+﻿namespace WET.lib.MonitorItems.Base
+{
+    public class BaseMonitorItem
+    {
+        public int ProcessID { get; internal set; }
+
+        public string ProcessName { get; internal set; }
+    }
+}
