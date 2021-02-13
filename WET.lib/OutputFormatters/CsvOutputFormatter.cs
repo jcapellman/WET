07648642e3933a0ecd1089d0ac9e11d9d899e305
Jarred Capellman <jcapellman@hotmail.com>
2021-02-12 20:43:22 -0600

    Added: CsvOutputFormatter class

---
+++ WET.lib/OutputFormatters/CsvOutputFormatter.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+using WET.lib.Enums;
+using WET.lib.OutputFormatters.Base;
+
+namespace WET.lib.OutputFormatters
+{
+    public class CsvOutputFormatter : BaseOutputFormatter
+    {
+        public override OutputFormat Formatter => OutputFormat.CSV;
+
+        public override string ConvertData(object item)
+        {
+            var properties = item.GetType().GetProperties();
+
+            var values = new List<string>();
+
+            foreach (var property in properties)
+            {
+                var val = property.GetValue(item);
+
+                if (val == null)
+                {
+                    values.Add(@"""");
+
+                    continue;
+                }
+
+                values.Add(val.ToString());
+            }
+
+            return string.Join(',', values);
+        }
+    }
+}
