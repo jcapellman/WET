0d7d3b3a29fb6e9e37b58e4abdf3bf2631300ff7
Jarred Capellman <jcapellman@hotmail.com>
2021-02-12 20:43:59 -0600

    Added: JsonOutputFormatter class

---
+++ WET.lib/OutputFormatters/JsonOutputFormatter.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+
+using WET.lib.Enums;
+using WET.lib.OutputFormatters.Base;
+
+namespace WET.lib.OutputFormatters
+{
+    public class JsonOutputFormatter : BaseOutputFormatter
+    {
+        public override OutputFormat Formatter => OutputFormat.JSON;
+
+        public override string ConvertData(object item) => JsonSerializer.Serialize(item);
+    }
+}
