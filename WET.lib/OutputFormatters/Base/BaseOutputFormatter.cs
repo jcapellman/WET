b78f1463df4556a61ad833a52ad69cca22aacb49
Jarred Capellman <jcapellman@hotmail.com>
2021-02-12 20:43:08 -0600

    Added: BaseOutputFormatter class

---
+++ WET.lib/OutputFormatters/Base/BaseOutputFormatter.cs
@@ -0,0 +1,11 @@
+﻿using WET.lib.Enums;
+
+namespace WET.lib.OutputFormatters.Base
+{
+    public abstract class BaseOutputFormatter
+    {
+        public abstract OutputFormat Formatter { get; }
+
+        public abstract string ConvertData(object item);
+    }
+}
