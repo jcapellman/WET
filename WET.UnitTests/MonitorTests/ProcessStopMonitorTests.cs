a8e7eb65240a049c32876577619be9b4a8fd2f6c
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 17:55:07 -0600

    Added: ProcessStopMonitorTests class #16

---
+++ WET.UnitTests/MonitorTests/ProcessStopMonitorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WET.lib.Enums;
+using WET.lib.Monitors;
+
+using WET.UnitTests.MonitorTests.Base;
+
+namespace WET.UnitTests.MonitorTests
+{
+    [TestClass]
+    public class ProcessStopMonitorTests : BaseMonitorTests
+    {
+        private readonly ProcessStopMonitor _monitor = new();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ProcessStopMonitor_NullTest()
+        {
+            _monitor.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ProcessStopMonitor_InvalidArgument()
+        {
+            _monitor.Parse(new TestEventData());
+        }
+
+        [TestMethod]
+        public void ProcessStopMonitor_Validity()
+        {
+            Assert.IsTrue(_monitor.MonitorType == MonitorTypes.ProcessStop);
+            Assert.IsTrue(_monitor.KeyWordMap == KernelTraceEventParser.Keywords.Process);
+        }
+    }
+}
