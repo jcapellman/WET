f9d9ac42953b0c0d1144a6149631b578c105b8d5
Jarred Capellman <jcapellman@hotmail.com>
2021-02-15 19:49:27 -0600

    Added: KernelTraceEvents Unit Tests #16

---
+++ WET.UnitTests/ExtensionTests/KernelTraceEventsTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WET.lib.Extensions;
+
+namespace WET.UnitTests.ExtensionTests
+{
+    [TestClass]
+    public class KernelTraceEventsTests
+    {
+        [TestMethod]
+        public void KernelTraceEvents_EmptyArgument()
+        {
+            var empty = new List<KernelTraceEventParser.Keywords>().ToKeywords();
+
+            Assert.IsNotNull(empty);
+            Assert.IsTrue(empty == KernelTraceEventParser.Keywords.None);
+        }
+
+        [TestMethod]
+        public void KernelTraceEvents_OneArgument()
+        {
+            var result = new List<KernelTraceEventParser.Keywords> {KernelTraceEventParser.Keywords.DiskIO}.ToKeywords();
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result == KernelTraceEventParser.Keywords.DiskIO);
+        }
+    }
+}
