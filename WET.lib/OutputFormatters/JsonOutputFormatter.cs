f995bebbe7a3f41249c4526be0c133ede6759db1
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 17:54:04 -0600

    Added: Null check in the JsonOutputFormatter class #16

--- WET.lib/OutputFormatters/JsonOutputFormatter.cs
+++ WET.lib/OutputFormatters/JsonOutputFormatter.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System;
+using System.Text.Json;
 
 using WET.lib.Enums;
 using WET.lib.OutputFormatters.Base;
@@ -9,6 +10,14 @@ namespace WET.lib.OutputFormatters
     {
         public override OutputFormat Formatter => OutputFormat.JSON;
 
-        public override string ConvertData(object item) => JsonSerializer.Serialize(item);
+        public override string ConvertData(object item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item), "object was null");
+            }
+
+            return JsonSerializer.Serialize(item);
+        } 
     }
 }
