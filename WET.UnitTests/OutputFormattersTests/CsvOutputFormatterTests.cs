25628541957bc2743a0307de64ed7fdacdd68c70
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 17:55:33 -0600

    Added: CsvOutputFormatterTests class #16

---
+++ WET.UnitTests/OutputFormattersTests/CsvOutputFormatterTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WET.lib.Enums;
+using WET.lib.MonitorItems;
+using WET.lib.OutputFormatters;
+
+namespace WET.UnitTests.OutputFormattersTests
+{
+    [TestClass]
+    public class CsvOutputFormatterTests
+    {
+        private readonly CsvOutputFormatter _csvFormatter = new();
+
+        [TestMethod]
+        public void CsvOutput_FormatterValidity()
+        {
+            Assert.IsTrue(_csvFormatter.Formatter == OutputFormat.CSV);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CsvOutput_NullTest()
+        {
+            _csvFormatter.ConvertData(null);
+        }
+
+        [TestMethod]
+        public void CsvOutput_ObjectTest()
+        {
+            var result = _csvFormatter.ConvertData(new ProcessStartMonitorItem());
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
