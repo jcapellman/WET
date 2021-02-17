2f755fa4073c6af319dd982d328a9352e9b19805
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 19:04:27 -0600

    Added: TcpSendMonitorTests class #16

---
+++ WET.UnitTests/MonitorTests/TcpSendMonitorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WET.lib.Enums;
+using WET.lib.Monitors;
+
+using WET.UnitTests.MonitorTests.Base;
+
+namespace WET.UnitTests.MonitorTests
+{
+    [TestClass]
+    public class TcpSendMonitorTests : BaseMonitorTests
+    {
+        private readonly TcpSendMonitor _monitor = new();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TcpSendMonitor_NullTest()
+        {
+            _monitor.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TcpSendMonitor_InvalidArgument()
+        {
+            _monitor.Parse(new TestEventData());
+        }
+
+        [TestMethod]
+        public void TcpSendMonitor_Validity()
+        {
+            Assert.IsTrue(_monitor.MonitorType == MonitorTypes.TcpSend);
+            Assert.IsTrue(_monitor.KeyWordMap == KernelTraceEventParser.Keywords.NetworkTCPIP);
+        }
+    }
+}
