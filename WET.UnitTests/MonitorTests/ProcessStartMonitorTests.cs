a36a0cc508d9eb74e6be60d9bd63f2ccb638984f
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 18:20:12 -0600

    Added: ProcessStartMonitorTests class #16

---
+++ WET.UnitTests/MonitorTests/ProcessStartMonitorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WET.lib.Enums;
+using WET.lib.Monitors;
+
+using WET.UnitTests.MonitorTests.Base;
+
+namespace WET.UnitTests.MonitorTests
+{
+    [TestClass]
+    public class ProcessStartMonitorTests : BaseMonitorTests
+    {
+        private readonly ProcessStartMonitor _monitor = new();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ProcessStartMonitor_NullTest()
+        {
+            _monitor.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ProcessStartMonitor_InvalidArgument()
+        {
+            _monitor.Parse(new TestEventData());
+        }
+
+        [TestMethod]
+        public void ProcessStartMonitor_Validity()
+        {
+            Assert.IsTrue(_monitor.MonitorType == MonitorTypes.ProcessStart);
+            Assert.IsTrue(_monitor.KeyWordMap == KernelTraceEventParser.Keywords.Process);
+        }
+    }
+}
