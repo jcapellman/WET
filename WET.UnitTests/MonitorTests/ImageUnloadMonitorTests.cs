a213730e63589f839a2e03d4c43955ef07ec2f11
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 18:42:01 -0600

    Added: ImageUnloadMonitorTests class #16

---
+++ WET.UnitTests/MonitorTests/ImageUnloadMonitorTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WET.lib.Enums;
+using WET.lib.Monitors;
+
+using WET.UnitTests.MonitorTests.Base;
+
+namespace WET.UnitTests.MonitorTests
+{
+    [TestClass]
+    public class ImageUnloadMonitorTests : BaseMonitorTests
+    {
+        private readonly ImageUnloadMonitor _monitor = new();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ImageUnloadMonitor_NullTest()
+        {
+            _monitor.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ImageUnloadMonitor_InvalidArgument()
+        {
+            _monitor.Parse(new TestEventData());
+        }
+
+        [TestMethod]
+        public void ImageUnloadMonitor_Validity()
+        {
+            Assert.IsTrue(_monitor.MonitorType == MonitorTypes.ImageUnload);
+            
+            Assert.AreEqual(KernelTraceEventParser.Keywords.ImageLoad, _monitor.KeyWordMap);
+        }
+    }
+}
