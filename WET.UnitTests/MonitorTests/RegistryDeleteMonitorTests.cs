cff9ef269a4edc7897bde509ecea5e21611abcae
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 18:46:35 -0600

    Added: RegistryDeleteMonitorTests class #16

---
+++ WET.UnitTests/MonitorTests/RegistryDeleteMonitorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WET.lib.Enums;
+using WET.lib.Monitors;
+
+using WET.UnitTests.MonitorTests.Base;
+
+namespace WET.UnitTests.MonitorTests
+{
+    [TestClass]
+    public class RegistryDeleteMonitorTests : BaseMonitorTests
+    {
+        private readonly RegistryDeleteMonitor _monitor = new();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RegistryDeleteMonitor_NullTest()
+        {
+            _monitor.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RegistryDeleteMonitor_InvalidArgument()
+        {
+            _monitor.Parse(new TestEventData());
+        }
+
+        [TestMethod]
+        public void RegistryDeleteMonitor_Validity()
+        {
+            Assert.IsTrue(_monitor.MonitorType == MonitorTypes.RegistryDelete);
+            Assert.IsTrue(_monitor.KeyWordMap == KernelTraceEventParser.Keywords.Registry);
+        }
+    }
+}
