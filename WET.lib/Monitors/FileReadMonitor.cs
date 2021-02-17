938cbbc5e6ed7d6fa27a14779ce8c527cc41a61f
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 18:17:56 -0600

    Change: Factored the FileReadMonitor class #16

--- WET.lib/Monitors/FileReadMonitor.cs
+++ WET.lib/Monitors/FileReadMonitor.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Diagnostics.Tracing;
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing;
 using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
 
 using WET.lib.Enums;
 using WET.lib.MonitorItems;
@@ -15,9 +18,11 @@ namespace WET.lib.Monitors
 
         public override MonitorTypes MonitorType => MonitorTypes.FileRead;
 
-        public override object ParseTraceEvent(TraceEvent eventData)
+        public override Type ExpectedEventDataType => typeof(DiskIOTraceData);
+
+        protected override object ParseTraceEvent(TraceEvent eventData)
         {
-            var obj = (Microsoft.Diagnostics.Tracing.Parsers.Kernel.DiskIOTraceData)eventData;
+            var obj = eventData as DiskIOTraceData;
 
             return new FileReadMonitorItem
             {
