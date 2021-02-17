dbbd6e69af22d2a555510c8e1c5e9a0d035ed0db
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 19:04:10 -0600

    Added: TcpReceiveMonitorTests class #16

---
+++ WET.UnitTests/MonitorTests/TcpReceiveMonitorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WET.lib.Enums;
+using WET.lib.Monitors;
+
+using WET.UnitTests.MonitorTests.Base;
+
+namespace WET.UnitTests.MonitorTests
+{
+    [TestClass]
+    public class TcpReceiveMonitorTests : BaseMonitorTests
+    {
+        private readonly TcpReceiveMonitor _monitor = new();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TcpReceiveMonitor_NullTest()
+        {
+            _monitor.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TcpReceiveMonitor_InvalidArgument()
+        {
+            _monitor.Parse(new TestEventData());
+        }
+
+        [TestMethod]
+        public void TcpReceiveMonitor_Validity()
+        {
+            Assert.IsTrue(_monitor.MonitorType == MonitorTypes.TcpReceive);
+            Assert.IsTrue(_monitor.KeyWordMap == KernelTraceEventParser.Keywords.NetworkTCPIP);
+        }
+    }
+}
