45d1dad8045fb18f4b67085091bcd265d9c25434
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 18:18:52 -0600

    Added: ImageLoadMonitorTests class #16

---
+++ WET.UnitTests/MonitorTests/ImageLoadMonitorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WET.lib.Enums;
+using WET.lib.Monitors;
+
+using WET.UnitTests.MonitorTests.Base;
+
+namespace WET.UnitTests.MonitorTests
+{
+    [TestClass]
+    public class ImageLoadMonitorTests : BaseMonitorTests
+    {
+        private readonly ProcessStopMonitor _monitor = new();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ImageLoadMonitor_NullTest()
+        {
+            _monitor.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ImageLoadMonitor_InvalidArgument()
+        {
+            _monitor.Parse(new TestEventData());
+        }
+
+        [TestMethod]
+        public void ImageLoadMonitor_Validity()
+        {
+            Assert.IsTrue(_monitor.MonitorType == MonitorTypes.ImageLoad);
+            Assert.IsTrue(_monitor.KeyWordMap == KernelTraceEventParser.Keywords.ImageLoad);
+        }
+    }
+}
