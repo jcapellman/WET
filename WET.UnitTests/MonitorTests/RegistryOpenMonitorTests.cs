ef68a638208702e7ac8862e5fc5056b8c7cf24e3
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 18:51:20 -0600

    Added: RegistryOpenMonitorTests class #16

---
+++ WET.UnitTests/MonitorTests/RegistryOpenMonitorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WET.lib.Enums;
+using WET.lib.Monitors;
+
+using WET.UnitTests.MonitorTests.Base;
+
+namespace WET.UnitTests.MonitorTests
+{
+    [TestClass]
+    public class RegistryOpenMonitorTests : BaseMonitorTests
+    {
+        private readonly RegistryOpenMonitor _monitor = new();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void RegistryOpenMonitor_NullTest()
+        {
+            _monitor.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void RegistryOpenMonitor_InvalidArgument()
+        {
+            _monitor.Parse(new TestEventData());
+        }
+
+        [TestMethod]
+        public void RegistryOpenMonitor_Validity()
+        {
+            Assert.IsTrue(_monitor.MonitorType == MonitorTypes.RegistryOpen);
+            Assert.IsTrue(_monitor.KeyWordMap == KernelTraceEventParser.Keywords.Registry);
+        }
+    }
+}
