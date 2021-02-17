9eeb584ad51f3a16750a905e15bec1e18e98e311
Jarred Capellman <jcapellman@hotmail.com>
2021-02-16 18:53:56 -0600

    Added: TcpConnectMonitorTests class #16

---
+++ WET.UnitTests/MonitorTests/TcpConnectMonitorTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WET.lib.Enums;
+using WET.lib.Monitors;
+
+using WET.UnitTests.MonitorTests.Base;
+
+namespace WET.UnitTests.MonitorTests
+{
+    [TestClass]
+    public class TcpConnectMonitorTests : BaseMonitorTests
+    {
+        private readonly TcpConnectMonitor _monitor = new();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TcpConnectMonitor_NullTest()
+        {
+            _monitor.Parse(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TcpConnectMonitor_InvalidArgument()
+        {
+            _monitor.Parse(new TestEventData());
+        }
+
+        [TestMethod]
+        public void TcpConnectMonitor_Validity()
+        {
+            Assert.IsTrue(_monitor.MonitorType == MonitorTypes.TcpConnect);
+            Assert.IsTrue(_monitor.KeyWordMap == KernelTraceEventParser.Keywords.NetworkTCPIP);
+        }
+    }
+}
