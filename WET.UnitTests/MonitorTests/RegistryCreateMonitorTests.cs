23855a65be0e2058b6f5d6c0be1936905e84e336
Jarred Capellman <jcapellman@hotmail.com>
2021-03-05 20:03:27 -0600

    Change: Updated the RegistryCreateMonitorTests with the new abstract methods

--- WET.UnitTests/MonitorTests/RegistryCreateMonitorTests.cs
+++ WET.UnitTests/MonitorTests/RegistryCreateMonitorTests.cs
@@ -19,14 +19,14 @@ namespace WET.UnitTests.MonitorTests
         [ExpectedException(typeof(ArgumentNullException))]
         public void RegistryCreateMonitor_NullTest()
         {
-            _monitor.Parse(null);
+            _monitor.ParseKernel(null);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void RegistryCreateMonitor_InvalidArgument()
         {
-            _monitor.Parse(new TestEventData());
+            _monitor.ParseKernel(new TestEventData());
         }
 
         [TestMethod]
