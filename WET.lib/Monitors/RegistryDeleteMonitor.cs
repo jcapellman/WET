aae54607f04cebf62d441572c47891debf4c28af
Jarred Capellman <jcapellman@hotmail.com>
2021-03-05 20:11:01 -0600

    Change: Updated the RegistryDeleteMonitor class to implement the updated BaseMonitor Abstract Class

--- WET.lib/Monitors/RegistryDeleteMonitor.cs
+++ WET.lib/Monitors/RegistryDeleteMonitor.cs
@@ -18,7 +18,7 @@ namespace WET.lib.Monitors
 
         public override Type ExpectedEventDataType => typeof(RegistryTraceData);
 
-        protected override object ParseTraceEvent(TraceEvent eventData)
+        protected override object ParseKernelTraceEvent(TraceEvent eventData)
         {
             var obj = eventData as RegistryTraceData;
 
@@ -28,5 +28,7 @@ namespace WET.lib.Monitors
                 ProcessID = obj.ProcessID
             };
         }
+
+        protected override object ParseTraceEvent(object eventData) => throw new NotImplementedException();
     }
 }
