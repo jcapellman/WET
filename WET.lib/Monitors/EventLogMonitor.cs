ce52bfaf162d7a4f64014651ef0135af73bc18db
Jarred Capellman <jcapellman@hotmail.com>
2021-03-05 20:13:44 -0600

    Added: Initial work on the EventLogMonitor class

---
+++ WET.lib/Monitors/EventLogMonitor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+
+using WET.lib.Enums;
+using WET.lib.MonitorItems;
+using WET.lib.Monitors.Base;
+
+namespace WET.lib.Monitors
+{
+    public class EventLogMonitor : BaseMonitor
+    {
+        public override KernelTraceEventParser.Keywords KeyWordMap => KernelTraceEventParser.Keywords.None;
+
+        public override MonitorTypes MonitorType => MonitorTypes.EventLogError;
+
+        public override Type ExpectedEventDataType => typeof(EventLogEntry);
+
+        public new bool KernelEventTracing = false;
+
+        protected override object ParseKernelTraceEvent(TraceEvent eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override object ParseTraceEvent(object eventData)
+        {
+            var obj = eventData as EventLogEntry;
+            
+            return new EventLogMonitorItem()
+            {
+                
+            };
+        }
+    }
+}
