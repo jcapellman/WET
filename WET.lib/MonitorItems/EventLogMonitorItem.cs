d80d353a794c92ca127a84b8320b0bc91ce65e34
Jarred Capellman <jcapellman@hotmail.com>
2021-03-05 20:35:18 -0600

    Added: Properties to the EventLogMonitorItem class

--- WET.lib/MonitorItems/EventLogMonitorItem.cs
+++ WET.lib/MonitorItems/EventLogMonitorItem.cs
@@ -1,9 +1,22 @@
-﻿using WET.lib.MonitorItems.Base;
+﻿using System;
+
+using WET.lib.MonitorItems.Base;
 
 namespace WET.lib.MonitorItems
 {
     public class EventLogMonitorItem : BaseMonitorItem
     {
+        
+        public string EntryType { get; internal set; }
+        
+        public string UserName { get; internal set; }
 
+        public long InstanceId { get; internal set; }
+
+        public string Message { get; internal set; }
+
+        public string Source { get; internal set; }
+
+        public DateTime TimeWritten { get; internal set; }
     }
 }
