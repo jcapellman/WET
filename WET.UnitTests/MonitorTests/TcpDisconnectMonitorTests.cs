2da2b32457d21bc23a11f8e22ddf0ce36742cfd0
Jarred Capellman <jcapellman@hotmail.com>
2021-03-05 20:04:45 -0600

    Change: Updated the TcpDisconnectMonitorTests with the new abstract methods

--- WET.UnitTests/MonitorTests/TcpDisconnectMonitorTests.cs
+++ WET.UnitTests/MonitorTests/TcpDisconnectMonitorTests.cs
@@ -19,14 +19,14 @@ namespace WET.UnitTests.MonitorTests
         [ExpectedException(typeof(ArgumentNullException))]
         public void TcpDisconnectMonitor_NullTest()
         {
-            _monitor.Parse(null);
+            _monitor.ParseKernel(null);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void TcpDisconnectMonitor_InvalidArgument()
         {
-            _monitor.Parse(new TestEventData());
+            _monitor.ParseKernel(new TestEventData());
         }
 
         [TestMethod]
