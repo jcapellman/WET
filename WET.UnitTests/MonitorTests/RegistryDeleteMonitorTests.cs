0006efb8e9e49bd88e16a889f6736d02e2307b2b
Jarred Capellman <jcapellman@hotmail.com>
2021-03-05 20:03:43 -0600

    Change: Updated the RegistryDeleteMonitorTests with the new abstract methods

--- WET.UnitTests/MonitorTests/RegistryDeleteMonitorTests.cs
+++ WET.UnitTests/MonitorTests/RegistryDeleteMonitorTests.cs
@@ -19,14 +19,14 @@ namespace WET.UnitTests.MonitorTests
         [ExpectedException(typeof(ArgumentNullException))]
         public void RegistryDeleteMonitor_NullTest()
         {
-            _monitor.Parse(null);
+            _monitor.ParseKernel(null);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void RegistryDeleteMonitor_InvalidArgument()
         {
-            _monitor.Parse(new TestEventData());
+            _monitor.ParseKernel(new TestEventData());
         }
 
         [TestMethod]
