62b81619c0f89de3579b783fa248ef2f7873ca73
Jarred Capellman <jcapellman@hotmail.com>
2021-03-05 20:05:14 -0600

    Change: Updated the TcpSendMonitorTests with the new abstract methods

--- WET.UnitTests/MonitorTests/TcpSendMonitorTests.cs
+++ WET.UnitTests/MonitorTests/TcpSendMonitorTests.cs
@@ -19,14 +19,14 @@ namespace WET.UnitTests.MonitorTests
         [ExpectedException(typeof(ArgumentNullException))]
         public void TcpSendMonitor_NullTest()
         {
-            _monitor.Parse(null);
+            _monitor.ParseKernel(null);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void TcpSendMonitor_InvalidArgument()
         {
-            _monitor.Parse(new TestEventData());
+            _monitor.ParseKernel(new TestEventData());
         }
 
         [TestMethod]
