a882be508e43b23aec40f54e53e995712f47a595
Jarred Capellman <jcapellman@hotmail.com>
2021-03-05 20:02:52 -0600

    Change: Updated the ProcessStartMonitorTests with the new abstract methods

--- WET.UnitTests/MonitorTests/ProcessStartMonitorTests.cs
+++ WET.UnitTests/MonitorTests/ProcessStartMonitorTests.cs
@@ -19,14 +19,14 @@ namespace WET.UnitTests.MonitorTests
         [ExpectedException(typeof(ArgumentNullException))]
         public void ProcessStartMonitor_NullTest()
         {
-            _monitor.Parse(null);
+            _monitor.ParseKernel(null);
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException))]
         public void ProcessStartMonitor_InvalidArgument()
         {
-            _monitor.Parse(new TestEventData());
+            _monitor.ParseKernel(new TestEventData());
         }
 
         [TestMethod]
