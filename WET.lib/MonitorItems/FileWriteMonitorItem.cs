390ab47aa1def79a1a14de4370b96b0544fed65f
Jarred Capellman <jcapellman@hotmail.com>
2021-09-22 18:54:59 -0500

    Added: FileWriteMonitor and FileWriteMonitorItem classes

---
+++ WET.lib/MonitorItems/FileWriteMonitorItem.cs
@@ -0,0 +1,9 @@
+﻿using WET.lib.MonitorItems.Base;
+
+namespace WET.lib.MonitorItems
+{
+    public class FileWriteMonitorItem : BaseMonitorItem
+    {
+        public string FileName { get; internal set; }
+    }
+}
---
+++ WET.lib/Monitors/FileWriteMonitor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
+
+using WET.lib.Enums;
+using WET.lib.MonitorItems;
+using WET.lib.Monitors.Base;
+
+namespace WET.lib.Monitors
+{
+    public class FileWriteMonitor : BaseMonitor
+    {
+        public override KernelTraceEventParser.Keywords KeyWordMap => KernelTraceEventParser.Keywords.DiskFileIO |
+                                                                      KernelTraceEventParser.Keywords.DiskIO |
+                                                                      KernelTraceEventParser.Keywords.FileIOInit |
+                                                                      KernelTraceEventParser.Keywords.FileIO;
+
+        public override MonitorTypes MonitorType => MonitorTypes.FileWrite;
+
+        public override Type ExpectedEventDataType => typeof(DiskIOTraceData);
+
+        protected override object ParseKernelTraceEvent(TraceEvent eventData)
+        {
+            var obj = eventData as DiskIOTraceData;
+
+            if (string.IsNullOrEmpty(obj.FileName))
+            {
+                return null;
+            }
+
+            return new FileWriteMonitorItem
+            {
+                FileName = obj.FileName,
+                ProcessID = obj.ProcessID
+            };
+        }
+
+        protected override object ParseTraceEvent(object eventData) => throw new NotImplementedException();
+    }
+}
