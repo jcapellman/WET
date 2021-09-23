5fbe59706e9939f8b2f6641fe62ec3c6d544a24f
Jarred Capellman <jcapellman@hotmail.com>
2021-09-22 19:06:03 -0500

    Added: FileDeleteMonitor and FileDeleteMonitorItem classes

---
+++ WET.lib/MonitorItems/FileDeleteMonitorItem.cs
@@ -0,0 +1,9 @@
+﻿using WET.lib.MonitorItems.Base;
+
+namespace WET.lib.MonitorItems
+{
+    public class FileDeleteMonitorItem : BaseMonitorItem
+    {
+        public string FileName { get; internal set; }
+    }
+}
---
+++ WET.lib/Monitors/FileDeleteMonitor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Parsers;
+using Microsoft.Diagnostics.Tracing.Parsers.Kernel;
+
+using WET.lib.Enums;
+using WET.lib.MonitorItems;
+using WET.lib.Monitors.Base;
+
+namespace WET.lib.Monitors
+{
+    public class FileDeleteMonitor : BaseMonitor
+    {
+        public override KernelTraceEventParser.Keywords KeyWordMap => KernelTraceEventParser.Keywords.DiskFileIO |
+                                                                      KernelTraceEventParser.Keywords.DiskIO |
+                                                                      KernelTraceEventParser.Keywords.FileIOInit |
+                                                                      KernelTraceEventParser.Keywords.FileIO;
+
+        public override MonitorTypes MonitorType => MonitorTypes.FileDelete;
+
+        public override Type ExpectedEventDataType => typeof(FileIONameTraceData);
+
+        protected override object ParseKernelTraceEvent(TraceEvent eventData)
+        {
+            var obj = eventData as FileIONameTraceData;
+
+            if (string.IsNullOrEmpty(obj.FileName))
+            {
+                return null;
+            }
+
+            return new FileDeleteMonitorItem
+            {
+                FileName = obj.FileName,
+                ProcessID = obj.ProcessID
+            };
+        }
+
+        protected override object ParseTraceEvent(object eventData) => throw new NotImplementedException();
+    }
+}
